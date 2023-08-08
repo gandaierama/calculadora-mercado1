ee76ae55584069d42d4c91598bdf22cbafd480d1
System Administrator <root@iMacDVV.local>
2023-08-08 06:10:19 -0300

    ft

--- robot1/src/app.module.ts
+++ robot1/src/app.module.ts
@@ -5,9 +5,20 @@ import { ScheduleModule } from '@nestjs/schedule';
 import { ConfigModule } from '@nestjs/config';
 import { ProdutosModule } from './produtos/produtos.module';
 import * as dotenv from 'dotenv';
+import {TypeOrmModule} from '@nestjs/typeorm';
 @Module({
   imports: [
     ScheduleModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'database',
+      port: 3306,
+      username: 'root',
+      password: 'test',
+      database: 'db_main',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
     ConfigModule.forRoot(),
     ProdutosModule,
   ],
