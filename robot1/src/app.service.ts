a8c18ef166e093d88f38b643ff9beae91e2534d3
System Administrator <root@iMacDVV.local>
2023-08-17 06:36:20 -0300

    auto

--- robot1/src/app.service.ts
+++ robot1/src/app.service.ts
@@ -42,7 +42,7 @@ export class AppService {
 
     puppeteer.use(StealthPlugin());
     this.logger.debug('Called when the current second is 45');
-    const URL = `https://pt.aliexpress.com/category/201003448/suits-sets.html`;
+    const URL = `https://pt.aliexpress.com/category/201003448/suits-sets.html?category_redirect=1`;
     const browser = await puppeteer.launch({
       args: [
         '--disable-gpu',
