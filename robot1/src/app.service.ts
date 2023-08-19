705df4b0c4312cb96e03230add26e31507c0fad1
System Administrator <root@iMacDVV.local>
2023-08-19 18:25:16 -0300

    auto

--- robot1/src/app.service.ts
+++ robot1/src/app.service.ts
@@ -39,7 +39,7 @@ export class AppService {
     return this.produtoRepository.save(obje);
   }
 
-  @Cron('0 3 * * * *')
+  @Cron('0 */3 * * * *')
   async handleCron() {
     this.intera=this.intera+1;
     puppeteer.use(StealthPlugin());
--- robot2/src/app.service.ts
+++ robot2/src/app.service.ts
@@ -23,7 +23,7 @@ export class AppService {
   private readonly logger = new Logger(AppService.name);
 
 
-  @Cron('0 3 * * * *')
+  @Cron('0 */3 * * * *')
   async handleCron() {
 
   
